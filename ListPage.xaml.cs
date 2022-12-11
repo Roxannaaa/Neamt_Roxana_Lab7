f60b37447dbc6bd4b1fc3f986eb0a451a396e358
Roxana Neamt <neamtroxana@yahoo.com>
2022-12-11 15:55:35 +0200

    4errors

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,6 +8,18 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -20,5 +32,13 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
     }
 }
